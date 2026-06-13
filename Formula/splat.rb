0baf4718ac78f12869fa852a8c0c9c2cd9287aac
Joseph Clarke <joemclarke@gmail.com>
2026-06-13 17:36:05 -0400

    Update splat formula to 1.0.93

--- Formula/splat.rb
+++ Formula/splat.rb
@@ -1,26 +1,26 @@
 class Splat < Formula
   desc "Splat command line interface for trading, auth, transfers, and automation"
   homepage "https://asksplat.com"
-  version "1.0.92"
+  version "1.0.93"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/protastudios/splat-cli-releases/releases/download/v1.0.92/splat_1.0.92_darwin_arm64.tar.gz"
-      sha256 "cea3a4efcbc611e2143a22214d3b646fd7c0aeae9450e3f4ab846298203b685e"
+      url "https://github.com/protastudios/splat-cli-releases/releases/download/v1.0.93/splat_1.0.93_darwin_arm64.tar.gz"
+      sha256 "05556b165862f3d66d87bf8cf53fefd278935ff6c2fcdde341749200edae5de7"
     else
-      url "https://github.com/protastudios/splat-cli-releases/releases/download/v1.0.92/splat_1.0.92_darwin_x64.tar.gz"
-      sha256 "8bf2eab51b126096aad7f2293ea60dae00f78c1ec7c8e69c99467acdfa432435"
+      url "https://github.com/protastudios/splat-cli-releases/releases/download/v1.0.93/splat_1.0.93_darwin_x64.tar.gz"
+      sha256 "5761a0936e27e2b1ca4fe7ffeee369d752c02623524c92218e27eb15b5228b73"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/protastudios/splat-cli-releases/releases/download/v1.0.92/splat_1.0.92_linux_arm64.tar.gz"
-      sha256 "1d5e93d37280ecb3713a3358b4391bc646b8548ce8f4600eaf6e718308272667"
+      url "https://github.com/protastudios/splat-cli-releases/releases/download/v1.0.93/splat_1.0.93_linux_arm64.tar.gz"
+      sha256 "0d897058a75198ad633fd470e2f495558f8c7125685e7873bbaaac0175acbd2c"
     else
-      url "https://github.com/protastudios/splat-cli-releases/releases/download/v1.0.92/splat_1.0.92_linux_x64_baseline.tar.gz"
-      sha256 "c6b7c0a628ddabacee56891cc73f3ccbd55ceed6f665bb50a369627fca60d2b2"
+      url "https://github.com/protastudios/splat-cli-releases/releases/download/v1.0.93/splat_1.0.93_linux_x64_baseline.tar.gz"
+      sha256 "15eac45e7c80f929ccf627fc1c16970109d9ebf9f318c12d417e5529ba0a2dca"
     end
   end
 
